33a176bbb394d6ff1bb30475a16dc64b648241a4
Seung-Goo KIM <dr.seunggoo.kim@gmail.com>
2024-09-09 13:09:51 +0200

    generatetoy+:add-signal-then-smoothing

--- code/tools/lea/private/generatetoy.m
+++ code/tools/lea/private/generatetoy.m
@@ -6,12 +6,12 @@ Y = {};
 for iSet = 1:Job.nSets
   X_ = normrnd(0, 1, [Job.nSamples, Job.nFeatures]);
   Y_ = normrnd(0, 1, [Job.nSamples, Job.nResponses]);
+  Y_(:,1) = Y_(:,1) + X_(:,1)*Job.EffectSize;
   if Job.TempGaussWin
     X_ = smoothdata(X_, 'gauss', Job.TempGaussWin);
     Y_ = smoothdata(Y_, 'gauss', Job.TempGaussWin);
   end
   X = [X, X_];
   Y = [Y, Y_];
-  Y{end}(:,1) = Y{end}(:,1) + X{end}(:,1)*Job.EffectSize;
 end
 end
